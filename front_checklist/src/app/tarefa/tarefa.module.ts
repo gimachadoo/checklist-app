a4e3d947a83ac2daab7acad1f504caf68ed8dc8e
gimachadoo <106285544+gimachadoo@users.noreply.github.com>
2023-05-04 22:17:59 -0300

    teste

--- front_checklist/src/app/tarefa/tarefa.module.ts
+++ front_checklist/src/app/tarefa/tarefa.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { TarefaRoutingModule } from './tarefa-routing.module';
 import { TarefaComponent } from './tarefa/tarefa.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 
 
 
